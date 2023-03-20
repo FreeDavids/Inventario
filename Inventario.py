a915b0238a98b8f793d917cad3eed056708a6b59
David Martin Sanchez <martinsanchezdavid27@gmail.com>
2023-03-20 23:32:28 +0100

    Primer commit vc

---
+++ Inventario.py
@@ -0,0 +1,13 @@
+bag = {
+    "Pan": 200,
+    "Agua": 100,
+    "Salchichon": 100,
+    "Queso": 100
+}
+
+def travel_in_time(hours):
+    for x in bag:
+        print(bag[x] - (50*hours))       
+        
+
+travel_in_time(1)
